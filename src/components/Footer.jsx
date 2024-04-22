027e6b0f89c30bb59ab5237c87f13d06fd8bddcd
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-22 10:19:11 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,8 +14,8 @@ function Footer() {
               <img src="images/foot.png" alt="" className=" h-[30px] sm:h-[110px] my-2 sm:my-8 sm:mx-0 mx-auto"  />
 
               <div className=" flex items-center gap-4 text-white mb-6 ml-5">
-                <a
-                  href="https://x.com/bonkk_3"
+              <a
+                  href="http://twitter.com/bonked420"
                   className=" text-xl text-black bg-white hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
                 >
                   <BsTwitterX />
@@ -24,7 +24,7 @@ function Footer() {
            
 
                 <a
-                  href="https://t.me/bonkeds" target="_blank"
+                  href="https://t.me/bonked420" target="_blank"
                   className=" text-xl text-black bg-white hover:bg-[#1a4093] rounded-full p-2 border-2 border-[#000]  hover:text-[#fff]"
                 >
                   <FaTelegram />
