6b7e35e875d413b775396432858aa8e102c70da2
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-22 09:53:26 +0530

    fff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <section className="hero-main relative overflow-x-hidden bg-[#ffbe2d] ">
   
-      <div className=" container-wrapper pt-[25%] sm:pt-[10%] ">
+      <div className=" container-wrapper pt-[25%] sm:pt-[6%] ">
         <div>
-          <img src="images/hero-txt.png" alt="" className=" mx-auto h-auto sm:h-[200px] w-auto my-[1.5%]" />
+          <img src="images/hero-txt.png" alt="" className=" mx-auto h-auto sm:h-[300px] w-auto my-[1.5%]" />
 
          
         </div>
